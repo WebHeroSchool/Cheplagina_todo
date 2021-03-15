3c575ef2339bfd054eaa090cc4594e6dbc661ebb
Надежда Чеплагина <makidaschka88@gmail.com>
2021-03-15 20:17:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+const flag = true;
+const count = 23;
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +11,31 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p style={{
+          color: 'red',
+          fontSize: 46
+        }}>
+        Hello World!
+        </p>
+        <p>
+        {count}
+        </p>
+        <p>
+        {flag && "Flag is true"}
+        {undefined}
+        {null}
+        {false}
+        {true}
+        <p>
+        {flag ? "true" : "false"}
+        </p>
+        <p>
+        {58}
+        </p>
+        <p>
+        {58-16}
+        </p>
         </p>
         <a
           className="App-link"
