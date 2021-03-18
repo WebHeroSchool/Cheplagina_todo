ac3fb8029ea72071345173a850bb7d6a06aa115e
Надежда Чеплагина <makidaschka88@gmail.com>
2021-03-18 12:49:48 +0300

    используя props вывело количество дел и в списке разные значения

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Footer = ({ count }) => (<p>Осталось выполнить вот столько дел: {}</p>);
+const Footer = ({ count }) => (<p>Осталось выполнить вот столько дел: {count}</p>);
 
 export default Footer;
