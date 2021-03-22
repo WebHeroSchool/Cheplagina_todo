400df1d8e7d982e34e71ce55eae1a496d3948618
Надежда Чеплагина <makidaschka88@gmail.com>
2021-03-22 13:47:51 +0300

    items перенесен в state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,34 +5,36 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 class App extends React.Component {
-	render() {
-		const items = [
-		{
-			value: 'Написать новое приложение',
-			isDone: true
-		},
-		{
-			value: 'Написать props-ы',
-			isDone: false
-		},
-		{
-			value: 'стилизовать',
-			isDone: false
-		},
-		{
-			value: 'Сделать все дела',
-			isDone: true
-		}
-	];
+		state = {
+			items: [
+					{
+						value: 'Написать новое приложение',
+						isDone: true
+					},
+					{
+						value: 'Написать props-ы',
+						isDone: false
+					},
+					{
+						value: 'стилизовать',
+						isDone: false
+					},
+					{
+						value: 'Сделать все дела',
+						isDone: true
+					}
+				]
+		};
 
-	return (
-		<div className={styles.wrap}>
-			<h1 className={styles.title}>Важные дела:</h1>
-			<InputItem />
-			<ItemList items={items} />
-			<Footer count={items.length} />
-		</div>);
-	}
+	render() {
+		return (
+			<div className={styles.wrap}>
+				<h1 className={styles.title}>Важные дела:</h1>
+				<InputItem />
+				<ItemList items={this.state.items} />
+				<Footer count={this.state.items.length} />
+			</div>);
+		}
 };
 
 export default App;
