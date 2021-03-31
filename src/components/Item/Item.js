f6a0a9046de629480c33089f28707b1c8a9534af
Nadezhda <makidaschka88@gmail.com>
2021-03-31 09:31:07 +0300

    использование таймера

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -8,13 +8,13 @@ import IconButton from '@material-ui/core/IconButton';
 
 class Item extends React.Component {
 	componentDidMount() {
-		console.log('componentDidMount');
+		this.timerID = setInterval(() => console.log('interval'), 1000);
 	}
 	componentDidUpdate() {
 		console.log('componentDidUpdate');
 	}
 	componentWillUnmount() {
-		console.log('componentWillUnmount');
+		clearInterval(this.timerID);
 	}
 
 	render() {
